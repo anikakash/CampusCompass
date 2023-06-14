4cdcd8b6bab1284cec86ec27b7e7fa543125ba14
Hm Elius Hossain Himel <himelahmed201617@gmail.com>
2023-06-14 23:05:16 +0600

    university information added

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php include("header.php"); ?>
 <?php include("dbcon.php"); ?>
 
+
 <div class="mx-auto" style="width: 80vw;">
     <form action="" method="GET">
         <div class="input-group mb-3">
@@ -8,8 +9,30 @@
             <button type="submit" class="btn btn-primary">Search</button>
         </div>
     </form>
+    <div class='university-list'>
+        <p>#1 Best University in Bangladesh</p>
+        <h2>Dhaka University</h2>
+        <div class="info" style="display: flex">
+            <h5><i class="fa-duotone fa-map-location"></i> Dhaka, Bangladesh</h5>
+            <h5><i class="fa-duotone fa-graduation-cap"></i> 4 Years</h4>
+            <h5><i class="fa-duotone fa-building-columns"></i> Public</h4>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis, possimus. Excepturi itaque repellat sed rem assumenda molestiae illo inventore magni corrupti saepe! Magni officia tempora, dolor minus ipsam voluptas amet cum praesentium nisi in, ex eius porro incidunt doloribus reiciendis consequuntur velit eligendi! Mollitia debitis totam illum, quos in expedita. <a href="/du">Learn More</a></p>
+        
+    </div>
+    <div class='university-list'>
+        <p>#2 Best University in Bangladesh</p>
+        <h2>Dhaka International University</h2>
+        <div class="info" style="display: flex">
+            <h5><i class="fa-duotone fa-map-location"></i> Banani, Dhaka, Bangladesh</h5>
+            <h5><i class="fa-duotone fa-graduation-cap"></i> 4 Years</h4>
+            <h5><i class="fa-duotone fa-building-columns"></i> Private</h4>
+        </div>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nobis, possimus. Excepturi itaque repellat sed rem assumenda molestiae illo inventore magni corrupti saepe! Magni officia tempora, dolor minus ipsam voluptas amet cum praesentium nisi in, ex eius porro incidunt doloribus reiciendis consequuntur velit eligendi! Mollitia debitis totam illum, quos in expedita. <a href="/du">Learn More</a></p>
+        
+    </div>
 
-    <table class="table table-hover table-bordered table-striped">
+    <!-- <table class="table table-hover table-bordered table-striped">
         <thead>
             <tr>
                 <th>ID</th>
@@ -68,7 +91,7 @@
             }
             ?>
         </tbody>
-    </table>
+    </table> -->
 </div>
 
 <?php include("footer.php"); ?>
