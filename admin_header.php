f8d769e53461a9e5f886e188da08f87f0956c10f
anikakash <anik15-14210@diu.edu.bd>
2023-06-19 12:51:12 +0600

    home url update

--- admin_header.php
+++ admin_header.php
@@ -20,12 +20,12 @@
 <div class="topbar">
         <div class="container">
             <header class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
-                <a href="./index.php" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
+                <a href="./" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
                 <img src="https://cdn.discordapp.com/attachments/1110463142485565450/1118166695392067734/Campus_Compass.png" height='150px' width="150px" alt="">
                 </a>
 
                 <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-                    <li><a href="./index.php" class="nav-link px-2 link-dark">Home</a></li>
+                    <li><a href="./" class="nav-link px-2 link-dark">Home</a></li>
                     <li><a href="./admin_home.php" class="nav-link px-2 link-dark">Admin</a></li>
                 </ul>
 
